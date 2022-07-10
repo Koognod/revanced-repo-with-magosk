20038af7d0211338a34ac02dc3ea07087fcfa182
Lacia~chan <laciadevel@proton.me>
2022-07-10 20:38:16 +0000

    Initial commit

---
+++ create-module.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+git clone https://github.com/Lyceris-chan/revanced-magisk-template
+
+cp build/revanced-root-signed.apk revanced-magisk-template
+cp com.google.android.youtube.apk revanced-magisk-template
+
+cd revanced-magisk-template
+
+zip -r ReVanced-v17.26.35-MagiskModule.zip ./*
+
+cd ..
+
+cp revanced-magisk-template/ReVanced-v17.26.35-MagiskModule.zip .
+
+rm -rf revanced-magisk-template
